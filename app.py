404ae204c5aa2e5a0afdab7407fc2aa2e73a22d9
kyeongsueong <67498838+kyeongsueong@users.noreply.github.com>
2023-01-16 00:29:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,9 @@ from flask import Flask, request, redirect, render_template, url_for, Response
 import cv2
 app = Flask(__name__)
 
-camera=cv2.VideoCapture(0)
 def generate_frames():
-    while True:
-
-        ## read the camera frame
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         
@@ -22,6 +13,6 @@ def index():
     getDatas = "hihi"
     photo = f"/img/1.jpeg"
     return render_template('./index.html', getDatas=getDatas, photo=photo)
-@app.route('/video')
+@app.route('/video', methods=["POST"])
 def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
