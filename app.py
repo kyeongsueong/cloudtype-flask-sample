730bd0545b95e583d67e51f464669c1be64b9b91
kyeongsueong <67498838+kyeongsueong@users.noreply.github.com>
2023-10-07 00:44:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, request, redirect, render_template, url_for, Response
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template, request
+from embedchain import App
+import os
 
+# Flask 애플리케이션 생성
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
+# OpenAI API 키 설정
+os.environ["OPENAI_API_KEY"] = "sdkfdicm"
 
-@app.route('/')
-def index():
-    return render_template("chat.html")
-@socketio.on('message')
-def handleMessage(msg):
-    send(msg, broadcast = True)
+# Embedchain App 객체 생성
+elon_bot = App()
 
+@app.route('/', methods=['GET', 'POST'])
+def chat_with_elon():
+    response = ""
 
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        response = elon_bot.query(user_input)
+
+    return render_template('chat.html', response=response)
+
+if __name__ == '__main__':
+    app.run()
